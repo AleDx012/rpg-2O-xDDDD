13a62e7d7dc6ad60a183f34407ad4acb3371701f
Alejandro <151110780+AleDx012@users.noreply.github.com>
2024-03-13 23:09:30 -0600

    Actualizar Player.cpp

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -6,55 +6,52 @@
 using namespace std;
 using namespace combat_utils;
 
-bool compareSpeed(Enemy* a, Enemy* b) {
+// Función para comparar la velocidad de dos enemigos
+bool compareSpeed(Enemy *a, Enemy *b) {
     return a->getSpeed() > b->getSpeed();
 }
 
+// Constructor de la clase Player
 Player::Player(string name, int health, int attack, int defense, int speed) : Character(name, health, attack, defense,
-    speed, true) {
+                                                                                        speed, true) {
     experience = 0;
     level = 1;
 }
 
-void Player::doAttack(Character* target) {
+// Método para que el jugador realice un ataque
+void Player::doAttack(Character *target) {
     int rolledAttack = getRolledAttack(getAttack());
     int trueDamage = target->getDefense() > rolledAttack ? 0 : rolledAttack - target->getDefense();
     target->takeDamage(trueDamage);
 }
 
+// Método para que el jugador reciba daño
 void Player::takeDamage(int damage) {
     setHealth(health - damage);
-    cout << "You have taken " << damage << " damage" << endl;
+    cout << "Has recibido " << damage << " puntos de daño" << endl;
     if (health <= 0) {
-        cout << "You have died" << endl;
+        cout << "Has muerto" << endl;
     }
 }
 
-void Player::flee(vector<Enemy*> enemies) {
+// Método para que el jugador huya del combate
+void Player::flee(vector<Enemy *> enemies) {
     std::sort(enemies.begin(), enemies.end(), compareSpeed);
-    Enemy* fastestEnemy = enemies[0];
-    bool fled = false;
-
-    // Comparar la velocidad del jugador con la del enemigo más rápido
+    Enemy *fastestEnemy = enemies[0];
+    bool huyo = false;
     if (this->getSpeed() > fastestEnemy->getSpeed()) {
-        fled = true;
-    }
-    else {
-        // Si el jugador no es más rápido, tiene una pequeña posibilidad de escapar
-        srand(time(NULL));
+        huyo = true;
+    } else {
+        srand(time(nullptr));
         int chance = rand() % 100;
-        cout << "Chance to escape: " << chance << "%" << endl;
-        fled = chance > 90; // Por ejemplo, el jugador tiene un 10% de posibilidades de escapar
+        cout << "Probabilidad de escapar: " << chance << "%" << endl;
+        huyo = chance > 97;
     }
 
-    if (fled) {
-        cout << "You managed to escape!" << endl;
-    }
-   
-void Player::emote() {
-    cout << "Jokes on you" << endl;
+    this->huyo = huyo;
 }
 
+// Método para subir de nivel al jugador
 void Player::levelUp() {
     level++;
     setHealth(getHealth() + 10);
@@ -63,6 +60,7 @@ void Player::levelUp() {
     setSpeed(getSpeed() + 5);
 }
 
+// Método para ganar experiencia
 void Player::gainExperience(int exp) {
     experience += exp;
     if (experience >= 100) {
@@ -71,48 +69,52 @@ void Player::gainExperience(int exp) {
     }
 }
 
-Character* Player::getTarget(vector<Enemy*> enemies) {
-    cout << "Choose a target" << endl;
+// Método para seleccionar un objetivo para atacar
+Character *Player::getTarget(vector<Enemy *> enemies) {
+    cout << "Elige un objetivo" << endl;
     int targetIndex = 0;
     for (int i = 0; i < enemies.size(); i++) {
         cout << i << ". " << enemies[i]->getName() << endl;
     }
     cin >> targetIndex;
-    //TODO: Add input validation
+    //TODO: Agregar validación de entrada
     return enemies[targetIndex];
 }
 
-Action Player::takeAction(vector<Enemy*> enemies) {
+// Método para que el jugador realice una acción en el combate
+Action Player::takeAction(const vector<Enemy *>& enemies) {
     int option = 0;
-    cout << "Choose an action" << endl;
-    cout << "1. Attack" << endl;
-    cout << "2. Flee" << endl;
+    cout << "Elige una acción" << endl;
+    cout << "1. Atacar" << endl;
+    cout << "2. Huir" << endl;
     cin >> option;
-    Character* target = nullptr;
+    Character *target = nullptr;
 
-    //Esta variable guarda
-    //1. Que voy a hacer?
-    //2. Con que velocidad/prioridad?
+    // Esta variable guarda
+    // 1. ¿Qué voy a hacer?
+    // 2. ¿Con qué velocidad/prioridad?
     Action myAction;
+    // 2.
     myAction.speed = this->getSpeed();
     myAction.subscriber = this;
 
     switch (option) {
-    case 1:
-        target = getTarget(enemies);
-        myAction.target = target;
-        myAction.action = [this, target]() {
-            doAttack(target);
+        case 1:
+            target = getTarget(enemies);
+            myAction.target = target;
+            // 1.
+            myAction.action = [this, target]() {
+                doAttack(target);
             };
-        break;
-    case 2:
-        myAction.action = [this, enemies]() {
-            flee(enemies);
+            break;
+        case 2:
+            myAction.action = [this, enemies]() {
+                flee(enemies);
             };
-        break;
-    default:
-        cout << "Invalid option" << endl;
-        break;
+            break;
+        default:
+            cout << "Opción inválida" << endl;
+            break;
     }
 
     return myAction;
